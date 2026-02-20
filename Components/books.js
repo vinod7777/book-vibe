6dcae23df9f69ef3d2918ef7b25f52f361c6bd22
lokeshkonna <lokeshkonna2007@gmail.com>
2026-02-20 23:39:28 +0530

    qwert

---
+++ Components/books.js
@@ -0,0 +1,30 @@
+const books = [
+    {
+        name: "The Great Gatsby",
+        price: 499,
+        type: "Fiction",
+        image: "../assets/recommendedimgs/storybook-1.webp",
+        stock: true
+    },
+    {
+        name: "To Kill a Mockingbird",
+        price: 399,
+        type: "Fiction",
+        image: "../assets/recommendedimgs/storybook-2.webp",
+        stock: true
+    },
+    {
+        name: "1984",
+        price: 299,
+        type: "Dystopian",
+        image: "../assets/recommendedimgs/storybook-3.webp",
+        stock: false
+    },
+    {
+        name: "Pride and Prejudice",
+        price: 349,
+        type: "Romance",
+        image: "../assets/recommendedimgs/storybook-4.webp",
+        stock: true
+    }
+];
