49fd89b20cf83f28411f7f59cfa65d90766a2112
lokeshkonna <lokeshkonna2007@gmail.com>
2026-02-21 11:59:38 +0530

    seacrh: 1

--- Components/books.js
+++ Components/books.js
@@ -26,5 +26,47 @@ const books = [
         type: "Romance",
         image: "../assets/recommendedimgs/storybook-4.webp",
         stock: true
+    },
+    {
+        name: "The Catcher in the Rye",
+        price: 279,
+        type: "Fiction",
+        image: "../assets/recommendedimgs/storybook-5.webp",
+        stock: false
+    },
+    {
+        name: "Artificial Intelligence: A Modern Approach",
+        price: 999,
+        type: "Academic",
+        image: "../assets/academicimages/ai.jpg",
+        stock: true
+    },
+    {
+        name: "Data Structures and Algorithm Analysis in C++",
+        price: 899,
+        type: "Academic",
+        image: "../assets/academicimages/dsa.jpg",
+        stock: true
+    },
+    {
+        name: "Machine Learning Yearning",
+        price: 799,
+        type: "Academic",
+        image: "../assets/academicimages/ml.jpg",
+        stock: true
+    },
+    {
+        name: "java: The Complete Reference",
+        price: 699,
+        type: "Academic",
+        image: "../assets/academicimages/java.jpg",
+        stock: true
+    },
+    {
+        name: "C++ Programming Language",
+        price: 599,
+        type: "Academic",
+        image: "../assets/academicimages/c++.jpg",
+        stock: true
     }
 ];
