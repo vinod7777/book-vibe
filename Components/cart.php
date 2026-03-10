12d3e7151238427333f33fa49d407d6a4b9e1638
pondurupurnachandrarao <pondurupurnachandrarao@gmail.com>
2026-03-10 17:54:11 +0530

    In cart page order-summary card adjusted.

--- Components/cart.php
+++ Components/cart.php
@@ -16,7 +16,7 @@
     body {
         background: #f4f4f4;
         padding-top: 100px;
-        font-family: sans-serif;
+        font-family:sans-serif;
     }
 
     .padding {
@@ -185,68 +185,14 @@ cursor:pointer;
 margin-top:20px;
 }
 
-<<<<<<< HEAD
-.price h2{
-    font-size:26px;
-}
-
-.price p{
-    color:gray;
-    font-size:14px;
-}
-
-.book-details{
-    width: 350px;
-    background:#f5f2f5;
-    border-radius:20px;
-    padding:50px;
-    box-shadow:0 5px 15px rgba(0,0,0,0.05);
-    border: 1px solid black;
-
-}
-
-.book-details h2{
-    margin-bottom:20px;
-}
-
-.yajnesh{
-    display:flex;
-    justify-content:space-between;
-    margin:15px 0;
-    color:#555;
-}
-
-.free{
-    color:green;
-    font-weight:600;
-}
-
-.total{
-    font-weight:700;
-    font-size:18px;
-    color:#000;
-}
-
-
-
-.continue{
-    width:100%;
-    padding:15px;
-    margin-top:50px;
-    border:none;
-    border-radius:30px;
-    background:#F26A21;
-    cursor:pointer;
-    transition: 0.5s all ease;
-    font-size: 20px;
-}
-
-.continue:hover{
-    background-color: #1a446cff;
-=======
 .shop-btn:hover{
-background:#0D3B66;
->>>>>>> d6443e37bbdcd1e3b7a1f2e7bae4c4eff49d8e37
+    background:#0D3B66;
+}
+.yajnesh{
+display:flex;
+justify-content:space-between;
+font-size:20px;
+
 }
 </style>
 </head>
@@ -294,7 +240,7 @@ background:#0D3B66;
 
         <div class="yajnesh">
             <span>Subtotal (<span id="items">1</span> items)</span>
-            <span id="subtotal">₹599</span>
+            <span id="subtotal">₹899</span>
         </div>
 
         <div class="yajnesh">
@@ -306,7 +252,7 @@ background:#0D3B66;
 
         <div class="yajnesh total">
             <span>Total</span>
-            <span id="total">₹599</span>
+            <span id="total">₹899</span>
         </div>
 
         <button class="continue">Buy Now!</button>
@@ -323,8 +269,6 @@ background:#0D3B66;
 </section>
 <?php include "footer.php"; ?>
 
-<<<<<<< HEAD
-=======
 <script>
 let count = 1;
 let price = 899;
@@ -369,7 +313,6 @@ function goHome(){
 window.location.href="index.php";
 
 }
-</script>
->>>>>>> d6443e37bbdcd1e3b7a1f2e7bae4c4eff49d8e37
+    </script>
 </body>
 </html>
