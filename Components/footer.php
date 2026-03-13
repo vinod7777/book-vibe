75c5267af47eb1b253f6b73db2b5ffd2f70a38a2
laxminarayana <laxminarayanamajji071@gmail.com>
2026-03-13 18:33:32 +0530

    fott

--- Components/footer.php
+++ Components/footer.php
@@ -5,7 +5,7 @@
         box-sizing: border-box;
     }
     footer {
-        margin-top: 40px;
+        margin-top: 70px;
         background-color: #0a1620;
         color: #fff;
         width: 100%;
